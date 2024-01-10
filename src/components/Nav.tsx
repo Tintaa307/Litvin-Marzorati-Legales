d6a6fe2f0727f99e3ce2cfc85d47c09ba553ceb5
Benzagon <gonbenzaquen@gmail.com>
2024-01-10 18:02:18 -0300

    feat: mobile hamburger menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,15 +6,23 @@ import Logo from '../../public/logo.svg';
 import Insta from '../../public/socials/insta.svg';
 import Linked from '../../public/socials/linkedin.svg';
 import useScrollPosition from "@/hooks/useScrollPosition";
+import { useState } from "react";
 
 const Nav = () => {   
     const scrollPosition = useScrollPosition()
+    const [isOpen, setIsOpen] = useState(false);
+
+    //Handles opening and closing of mobile hamburger menu
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div className={`w-screen h-20 fixed top-0 z-50 main-padding flex items-center justify-between duration-200 ${scrollPosition > 20 ? 'bg-background' : "bg-transparent"}`}>
             <Link href={'/'}>
                 <Image src={Logo} alt="Logo" className="w-32"></Image>
             </Link>
+            {/* For Desktop */}
             <div className="md:flex hidden items-center gap-6">
                 <Link href={'/'}>
                     <h3 className="text-base font-medium">Inicio</h3>
@@ -38,6 +46,18 @@ const Nav = () => {
                 <Link href={'https://www.instagram.com/lmlegales/'} target="_blank">
                     <Image src={Linked} alt="LinkedIn socials"></Image>
                 </Link>
+            </div>
+            {/* For mobile */}
+            <button onClick={handleClick} className="md:hidden flex flex-col justify-center items-center">
+                <span className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-1'}`}></span>
+                <span className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+                <span className={`bg-black block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45' : 'translate-y-1'}`}></span>
+            </button>
+            <div className={`absolute md:hidden h-screen w-[70%] top-0 bg-background transition-all ease-out duration-300 ${isOpen ? 'left-0' : '-left-72 sm:-left-[32rem]'}`}>
+                <div className="w-full h-full bg-background flex flex-col p-10">
+                    <Link href={'./'}>Inicio</Link>
+                    <Link href={'./about'}>Sobre nosotros</Link>
+                </div>
             </div>
         </div>
     )
