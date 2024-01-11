e73abc8bd9900c46b0c781085323fc0a9313a811
Benzagon <gonbenzaquen@gmail.com>
2024-01-11 11:50:22 -0300

    fix: added finalCTA to service
    forgot to add finalcta to service page

--- src/components/sections/servicios/ServiceBody.tsx
+++ src/components/sections/servicios/ServiceBody.tsx
@@ -1,3 +1,5 @@
+import FinalCTA from "../landing/FinalCTA";
+
 interface Props {
     firstHighlight?: string,
     secondHighlight?: string,
@@ -11,25 +13,28 @@ interface Props {
 
 const ServiceBody = ({firstHighlight, secondHighlight, p1, p2, p3, p4, p5}: Props) => {
     return (
-        <div className="main-padding pb-[6.25rem] flex flex-col gap-[6.25rem] font-poppins">
-            {firstHighlight && (
-                <div className="py-5 border-t border-b border-black">
-                    <h2 className="text-justify md:text-[1.5rem] text-[1.25rem] font-semibold">{firstHighlight}</h2>
+        <>
+            <div className="main-padding sm:pb-[6.25rem] pb-[5.5rem] flex flex-col gap-[6.25rem] font-poppins">
+                {firstHighlight && (
+                    <div className="py-5 border-t border-b border-black">
+                        <h2 className="text-justify md:text-[1.5rem] text-[1.25rem] font-semibold">{firstHighlight}</h2>
+                    </div>
+                )}
+                <div className="text-base font-medium text-justify flex flex-col gap-6">
+                    <p>{p1}</p>
+                    <p>{p2}</p>
+                    <p>{p3}</p>
+                    <p>{p4}</p>
+                    <p>{p5}</p>
                 </div>
-            )}
-            <div className="text-base font-medium text-justify flex flex-col gap-6">
-                <p>{p1}</p>
-                <p>{p2}</p>
-                <p>{p3}</p>
-                <p>{p4}</p>
-                <p>{p5}</p>
+                {secondHighlight && (
+                    <div className="py-5 border-t border-b border-black">
+                        <h2 className="text-justify md:text-[1.5rem] text-[1.25rem] font-semibold">{secondHighlight}</h2>
+                    </div>
+                )}
             </div>
-            {secondHighlight && (
-                <div className="py-5 border-t border-b border-black">
-                    <h2 className="text-justify md:text-[1.5rem] text-[1.25rem] font-semibold">{secondHighlight}</h2>
-                </div>
-            )}
-        </div>
+            <FinalCTA></FinalCTA>
+        </>
     );
 };
 
