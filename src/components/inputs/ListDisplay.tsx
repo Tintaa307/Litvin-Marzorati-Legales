2c6ad8de997a11e12d27d97d091c3e6d9335a15c
Benzagon <gonbenzaquen@gmail.com>
2024-01-24 12:45:45 -0300

    feat: listdisplayer
    created component for mobile menu that allows animated list displaying

---
+++ src/components/inputs/ListDisplay.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import { useState } from "react";
+
+interface Props {
+    heading: string,
+    options: { title: string; url: string; }[]
+}
+
+const ListDisplay = ({heading, options}: Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    //Handles opening and closing of list
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="flex flex-col gap-4">
+            <div onClick={handleClick} className="flex gap-2 items-center group">
+                <h2>{heading}</h2>
+                <h2 className={`transition-all ${!isOpen ? 'rotate-0' : 'rotate-90'}`}>{`>`}</h2>
+            </div>
+            <AnimatePresence>
+                {   
+                isOpen &&
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        className="flex flex-col gap-4 pl-2">
+                        {
+                            options.map((service) => (
+                                <Link className="text-sm" href={service.url}>{service.title}</Link>
+                            ))
+                        }
+                    </motion.div>
+                }
+           </AnimatePresence>
+        </div>
+    );
+};
+
+export default ListDisplay;
--- src/components/navigation/MobileMenu.tsx
+++ src/components/navigation/MobileMenu.tsx
@@ -4,6 +4,7 @@ import Insta from '../../../public/socials/insta.svg';
 import Linked from '../../../public/socials/linkedin.svg';
 import Facebook from '../../../public/socials/facebook.svg';
 import Image from "next/image";
+import ListDisplay from "../inputs/ListDisplay";
 
 interface Props {
     scrollPosition: number,
@@ -12,14 +13,34 @@ interface Props {
 }
 
 const MobileMenu = ({scrollPosition, isOpen, white}: Props) => {
+    const Services = [
+        {
+            title: 'Asesoramiento legal en metavero e IA',
+            url: '/servicios/metaverso-e-ia'
+        },
+        {
+            title: 'Asesoramiento en propiedad intelectual',
+            url: '/servicios/propiedad-intelectual'
+        },
+        {
+            title: 'Asesoramiento en internet',
+            url: '/servicios/internet'
+        },
+        {
+            title: 'Asesoramiento legal general',
+            url: '/servicios/asesoramiento-general'
+        },
+        {
+            title: 'Emprendedores y nuevos proyectos',
+            url: '/servicios/emprendedores'
+        },
+    ];
+
     return (
     <div className="w-full h-full flex flex-col gap-[1.25rem] p-[0.62rem] text-base font-medium text-black">
         <Link href={'/'}>Inicio</Link>
         <Link href={'/about'}>Sobre nosotros</Link>
-        <div className="flex gap-2 items-center">
-            <h2>Servicios</h2>
-            <h2 className="rotate-0">{`>`}</h2>
-        </div>
+        <ListDisplay heading="Servicios" options={Services}/>
         <Link href={'/'}>Contacto</Link>
         <span className="w-full h-[1px] bg-black/30"></span>
         <div className="flex gap-2 items-center">
