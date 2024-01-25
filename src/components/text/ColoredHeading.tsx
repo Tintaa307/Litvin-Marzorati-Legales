0ac73d3189b1f0aa3a9cb1e2b833f901ba524175
Benzagon <gonbenzaquen@gmail.com>
2024-01-24 22:55:12 -0300

    feat: color changing contact
    tried using background image. megh

--- src/components/text/ColoredHeading.tsx
+++ src/components/text/ColoredHeading.tsx
@@ -7,10 +7,10 @@ interface Props {
 const ColoredHeading = ({title, notCentered, poppins}: Props) => {
     return (
         <div className="relative">
-            <div className={`text-black opacity-100 flex flex-col gap-5 max-w-[44rem] ${notCentered ? 'md:text-left text-center' : 'text-center'}`}>
+            <div className={`relative font-bold text-transparent bg-no-repeat bg-center bg-[url('/assets/plainAccent.png')] animate-moveBg bg-clip-text opacity-100 flex z-30 flex-col gap-5 max-w-[44rem] ${notCentered ? 'md:text-left text-center' : 'text-center'}`}>
                 <h2 className={`sm:text-[42px] text-[2rem] ${!poppins ? 'font-dmSerif font-normal' : 'font-poppins font-medium'}`}>{title}</h2>
             </div>
-            <div className={`absolute top-0 overflow-hidden opacity-0 w-[100%] text-transparent bg-clip-text bg-accent-orange flex flex-col gap-5 max-w-[44rem] ${notCentered ? 'md:text-left text-center' : 'text-center'}`}>
+            <div className={`absolute text-black/60 top-0 flex z-10 flex-col gap-5 max-w-[44rem] ${notCentered ? 'md:text-left text-center' : 'text-center'}`}>
                 <h2 className={`sm:text-[42px] text-[2rem] ${!poppins ? 'font-dmSerif font-normal' : 'font-poppins font-medium'}`}>{title}</h2>
             </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
       animation: {
         marquee: 'marquee 30s linear infinite',
         marquee2: 'marquee2 30s linear infinite',
-        textColor: 'expandDiv 20s linear infinite',
+        moveBg: 'moveBg 20s ease infinite',
       },
       keyframes: {
         marquee: {
@@ -39,10 +39,10 @@ const config: Config = {
           '0%': { transform: 'translateX(100%)' },
           '100%': { transform: 'translateX(0%)' },
         },
-        expandDiv: {
-          '0%': { width: '0%' },
-          '50%': { width: '100%' },
-          '100%': { width: '0%' },
+        moveBg: {
+          '0%': { 'background-position': '0% 0%' },
+          '50%': { 'background-position': '800%' },
+          '100%': { 'background-position': '0% 0%' },
         },
       },
       screens: {
