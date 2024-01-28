505916a2044b4db141a7c8f6299c98fad24ebdb5
Benzagon <gonbenzaquen@gmail.com>
2024-01-28 16:53:03 -0300

    feat: contact btn link

--- src/components/inputs/Button.tsx
+++ src/components/inputs/Button.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 const CtaButton = () => {
     return (
-        <button className="sm:px-8 sm:py-3 px-6 py-2 rounded-lg bg-gradient-to-r from-accent-brown from-[-39.43%] to-accent-orange to-162%">
+        <Link href={'contacto'} className="sm:px-8 sm:py-3 px-6 py-2 rounded-lg bg-gradient-to-r from-accent-brown from-[-39.43%] to-accent-orange to-162%">
             <h3 className="font-medium sm:text-lg sm:text-md">Contáctanos</h3>
-        </button>
+        </Link>
     );
 };
 
