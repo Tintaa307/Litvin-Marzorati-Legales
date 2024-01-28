e2ee71e2319fce6334816a0d65d37d7e8076714e
Benzagon <gonbenzaquen@gmail.com>
2024-01-28 14:19:34 -0300

    fix: hidden env variables

--- src/app/_actions.ts
+++ src/app/_actions.ts
@@ -2,10 +2,10 @@
 import nodemailer from 'nodemailer';
 
 export async function sendEmail({email, name, subject, message}: {email: string, name: string, subject?: string, message: string}){
-    const HOST = process.env.NEXT_PUBLIC_HOST;
-    const EMAIL = process.env.NEXT_PUBLIC_EMAIL;
-    const PASS = process.env.NEXT_PUBLIC_PASS;
-    const EMAILTO = process.env.NEXT_PUBLIC_TOEMAIL;
+    const HOST = process.env.HOST;
+    const EMAIL = process.env.EMAIL;
+    const PASS = process.env.PASS;
+    const EMAILTO = process.env.TOEMAIL;
     
     // Conection with nodemailer
     const mailTransporter = nodemailer.createTransport({
