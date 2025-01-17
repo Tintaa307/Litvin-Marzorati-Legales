451c4e7b9ac0d380a32cedc1e48574e4d014beda
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2025-01-17 17:06:40 -0300

    feat: delete newsletter

--- src/actions/newsletter-action.ts
+++ src/actions/newsletter-action.ts
@@ -1,37 +1,37 @@
-"use server"
+// "use server"
 
-import { NewsletterSchema } from "@/lib/validations/Forms"
-import axios from "axios"
-import { z } from "zod"
+// import { NewsletterSchema } from "@/lib/validations/Forms"
+// import axios from "axios"
+// import { z } from "zod"
 
-export const handleSubmit = async (formData: FormData) => {
-  const email = formData.get("email")
+// export const handleSubmit = async (formData: FormData) => {
+//   const email = formData.get("email")
 
-  const values = {
-    email: email as string,
-  }
+//   const values = {
+//     email: email as string,
+//   }
 
-  if (Object.values(values).some((value) => value === "")) {
-    return { status: 500, message: "Por favor, complete todos los campos" }
-  }
+//   if (Object.values(values).some((value) => value === "")) {
+//     return { status: 500, message: "Por favor, complete todos los campos" }
+//   }
 
-  try {
-    const result = NewsletterSchema.parse(values)
-    const res = await axios.post(
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/api/newsletter"
-        : "https://lmlegales.com.ar/api/newsletter",
-      result
-    )
+//   try {
+//     const result = NewsletterSchema.parse(values)
+//     const res = await axios.post(
+//       process.env.NODE_ENV === "development"
+//         ? "http://localhost:3000/api/newsletter"
+//         : "https://lmlegales.com.ar/api/newsletter",
+//       result
+//     )
 
-    return res.data
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      const errors = error.errors.map((err) => err.message)
-      return { status: 500, message: errors }
-    } else {
-      console.log(error)
-      return error
-    }
-  }
-}
+//     return res.data
+//   } catch (error) {
+//     if (error instanceof z.ZodError) {
+//       const errors = error.errors.map((err) => err.message)
+//       return { status: 500, message: errors }
+//     } else {
+//       console.log(error)
+//       return error
+//     }
+//   }
+// }
--- src/app/api/newsletter/route.ts
+++ src/app/api/newsletter/route.ts
@@ -1,55 +1,55 @@
-import { Resend } from "resend"
-import { NextResponse } from "next/server"
-import { NewsletterTemplate } from "@/components/newsletter-template"
+// import { Resend } from "resend"
+// import { NextResponse } from "next/server"
+// import { NewsletterTemplate } from "@/components/newsletter-template"
 
-export async function POST(req: Request) {
-  if (req.method !== "POST") {
-    return NextResponse.json({ message: "Method not allowed", status: 405 })
-  }
+// export async function POST(req: Request) {
+//   if (req.method !== "POST") {
+//     return NextResponse.json({ message: "Method not allowed", status: 405 })
+//   }
 
-  try {
-    const resend = new Resend(process.env.RESEND_API_KEY)
+//   try {
+//     const resend = new Resend(process.env.RESEND_API_KEY)
 
-    const data = await req.json()
+//     const data = await req.json()
 
-    if (!data) {
-      return NextResponse.json({
-        message: "No data provided",
-        status: 400,
-      })
-    }
+//     if (!data) {
+//       return NextResponse.json({
+//         message: "No data provided",
+//         status: 400,
+//       })
+//     }
 
-    const { email } = data
+//     const { email } = data
 
-    const { data: emailData, error } = await resend.emails.send({
-      from: "LMLegales <contacto@lmlegales.com.ar>",
-      to: ["info@lmlegales.com.ar"],
-      subject: "Nuevo mensaje desde LMLegales Website",
-      react: NewsletterTemplate({
-        email: email,
-      }),
-      text: "",
-    })
+//     const { data: emailData, error } = await resend.emails.send({
+//       from: "LMLegales <contacto@lmlegales.com.ar>",
+//       to: ["info@lmlegales.com.ar"],
+//       subject: "Nuevo mensaje desde LMLegales Website",
+//       react: NewsletterTemplate({
+//         email: email,
+//       }),
+//       text: "",
+//     })
 
-    if (error) {
-      console.error(error)
-      return NextResponse.json({
-        message: "Error al enviar el mensaje.",
-        status: 500,
-      })
-    } else {
-      console.log(emailData)
-    }
+//     if (error) {
+//       console.error(error)
+//       return NextResponse.json({
+//         message: "Error al enviar el mensaje.",
+//         status: 500,
+//       })
+//     } else {
+//       console.log(emailData)
+//     }
 
-    return NextResponse.json({
-      message: "El mensaje ha sido enviado correctamente.",
-      status: 200,
-    })
-  } catch (error) {
-    console.error(error)
-    return NextResponse.json({
-      message: "Error al enviar el mensaje.",
-      status: 500,
-    })
-  }
-}
+//     return NextResponse.json({
+//       message: "El mensaje ha sido enviado correctamente.",
+//       status: 200,
+//     })
+//   } catch (error) {
+//     console.error(error)
+//     return NextResponse.json({
+//       message: "Error al enviar el mensaje.",
+//       status: 500,
+//     })
+//   }
+// }
