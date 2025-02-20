3b837900ed859652fd21d58949f3acfd23223d0c
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2025-02-20 20:54:44 -0300

    feat: add chatbot

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,5 @@
 import { streamText } from "ai"
-import { google } from "@ai-sdk/google"
+import { openai } from "@ai-sdk/openai"
 import { NextResponse } from "next/server"
 import { responses } from "@/lib/chat/responses"
 
@@ -9,12 +9,12 @@ export async function POST(request: Request) {
 
     const stream = await streamText({
       //@ts-ignore
-      model: google("gemini-1.5-pro"),
+      model: openai("gpt-4o"),
       system: `
-        Usted es un asistente útil que brinda información sobre los servicios de Litvin-Marzorati Legales o tambien LML Legales. Tienes que ser amable e informativo. Puede proporcionar información sobre los servicios, programar una reunión o proporcionar información de contacto que se encuentra en el archivo ${responses}. Si el usuario solicita algo que no está en el archivo, puede proporcionar un mensaje predeterminado como "Lo siento, no tengo una respuesta para eso."
+        Usted es un asistente útil que brinda información sobre los servicios de Litvin-Marzorati Legales o tambien LML Legales. Tienes que ser amable e informativo. Puede proporcionar información sobre los servicios, programar una reunión o proporcionar información de contacto que se encuentra en el archivo ${responses}. Si el usuario solicita algo que no está en el archivo, puede proporcionar un mensaje predeterminado como "Lo siento, no tengo una respuesta para eso. Tambien puedes proporcionar informacion acerca de los numeros de clases del 1 al 45. Que numero representa la clase que desea registrar el usuario etc."
 
-        You are a useful assistant who provides information about the services of Litvin-Marzorati Legales or also LML Legales. You have to be friendly and informative. You can provide information about services, schedule a meeting, or provide contact information found in the ${responses} file. If the user requests something that is not in the file, you can provide a default message such as "Sorry, I don't have an answer for that.
-        
+        You are a useful assistant who provides information about the services of Litvin-Marzorati Legales or also LML Legales. You have to be friendly and informative. You can provide information about services, schedule a meeting, or provide contact information found in the ${responses} file. If the user requests something that is not in the file, you can provide a default message such as "Sorry, I don't have an answer for that. You can also provide information about the numbers of classes from 1 to 45. Which number represents the class that the user wants to register etc."
+
         Las redes sociales de Litvin-Marzorati Legales son:
         - Facebook: https://www.facebook.com/Lmlegales
         - Instagram: https://www.instagram.com/lmlegales/
@@ -24,14 +24,12 @@ export async function POST(request: Request) {
         - info@lmlegales.com.ar
 
         Teléfono:
-        - +54 9 11 4081-4887 
+        - +54 9 11 4081-4887
       `,
       maxTokens: 100,
       messages,
       tools: {},
     })
-
-    console.log(stream.toAIStreamResponse())
 
     return stream.toAIStreamResponse()
   } catch (error) {
